482920f755b4cf64d788a5cd94d40f55ca6ffe2a
Максим Повнич <maxympovnych@gmail.com>
2025-11-25 00:45:04 +0200

    create grouping oltp

--- scripts/grouping.sql
+++ scripts/grouping.sql
@@ -5,3 +5,10 @@ GROUP BY brand;
 SELECT type, AVG(mileage) AS avg_maintenance_mileage
 FROM maintenance
 GROUP BY type;
+
+SELECT
+   EXTRACT(YEAR FROM registration_date) AS registration_year,
+   COUNT(*) AS new_users
+FROM users
+GROUP BY EXTRACT(YEAR FROM registration_date)
+ORDER BY registration_year;
