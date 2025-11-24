e8565d568f7b4b7d21d141d82cc4b293af6adbcd
Максим Повнич <maxympovnych@gmail.com>
2025-11-25 00:44:25 +0200

    create group filtering oltp

--- scripts/goup filtering.sql
+++ scripts/goup filtering.sql
@@ -8,3 +8,15 @@ SELECT vehicle_id, COUNT(*) AS trip_count
 FROM trip
 GROUP BY vehicle_id
 HAVING COUNT(*) >= 3;
+
+SELECT user_id, AVG(distance) AS avg_distance
+FROM trip
+GROUP BY user_id
+HAVING AVG(distance) > 10;
+
+
+SELECT user_id, COUNT(*) AS cancelled_count
+FROM booking
+WHERE status = 'cancelled'
+GROUP BY user_id
+HAVING COUNT(*) > 1;
